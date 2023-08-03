5c78ddfe64c961ee27dfd1ebf61a6dd306248a6b
BKaya44 <11460681+BKaya44@users.noreply.github.com>
2023-08-03 22:26:14 +0200

    Fix SECRET_KEY

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.post('/login', async (req, res) => {
                 id: user.id,
                 username: user.username,
             },
-            SECRET_KEY,
+            process.env.SECRET_KEY,
             { expiresIn: '1h' }
         );
 
